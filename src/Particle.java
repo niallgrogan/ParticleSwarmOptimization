12fe75635ed1f71c81e05c110be444bf1c46f760
Niall Grogan <n.grogan2@nuigalway.ie>
2015-11-17 09:14:37 +0000

    Setting up PSO project

---
+++ src/Particle.java
@@ -0,0 +1,18 @@
+/**
+ * Created by Niall on 13/11/2015.
+ */
+public class Particle {
+    int px;
+    int py;
+    int vx;
+    int vy;
+
+    public Particle()
+    {
+        px = 0;
+        py = 0;
+        vx = 0;
+        vy = 0;
+    }
+
+}
