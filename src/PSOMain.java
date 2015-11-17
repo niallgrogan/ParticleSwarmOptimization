72efe8deba9ba25bb71506b39a264b4af6c8aa70
Niall Grogan <n.grogan2@nuigalway.ie>
2015-11-17 09:17:22 +0000

    Setting up PSO project

--- src/PSOMain.java
+++ src/PSOMain.java
@@ -1,16 +1,7 @@
-/**
- * Created by Niall on 13/11/2015.
- */
+
 public class PSOMain {
     public static void main(String[] Args)
     {
-        Particle[] particles = new Particle[5];
-        particles[0] = new Particle();
-        particles[1] = new Particle();
-        particles[2] = new Particle();
-        particles[3] = new Particle();
-        particles[4] = new Particle();
 
-        System.out.println(particles[0].px);
     }
 }
