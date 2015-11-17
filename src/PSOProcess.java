ec2620964c0f9fbcce6d89833aed261d99fe82c7
Niall Grogan <n.grogan2@nuigalway.ie>
2015-11-17 11:19:45 +0000

    Implemented PSO for swarm of 50 and 10 iterations, everything seems to be working BUT we have a poorly defined fitness function, therefore cannot be sure

--- src/PSOProcess.java
+++ src/PSOProcess.java
@@ -8,7 +8,11 @@ public class PSOProcess {
     Vector<Particle> swarm = new Vector<Particle>(50);
     Vector<Position> bestPositions = new Vector<Position>(50);
     int globalBestIndex;
-    double fitness=0;
+    double globalFitness=0;
+    double newPosX, newPosY, newVelX, newVelY;
+    double r1,r2;
+    double c1, c2 = 1.05;
+    double pbestX, pbestY, gbestX, gbestY;
 
     public PSOProcess() {}
 
@@ -25,9 +29,9 @@ public class PSOProcess {
     private void findGBest() {
         for(int i=0; i<swarm.capacity(); i++)
         {
-            if(swarm.elementAt(i).getP().getX() > fitness)
+            if(swarm.elementAt(i).getP().getX() > globalFitness)
             {
-                fitness = swarm.elementAt(i).getP().getX();
+                globalFitness = swarm.elementAt(i).getP().getX();
                 globalBestIndex = i;
             }
         }
@@ -35,35 +39,44 @@ public class PSOProcess {
 
     public void execute() {
         this.initialise();
-        for(int i=0; i<swarm.capacity(); i++)
-        {
-            System.out.print(swarm.elementAt(i).getP().getX()+"["+i+"],\n");
+
+        for (int j=0; j<10; j++) {
+            for(int i=0; i<swarm.capacity(); i++)
+            {
+                r1 = Math.random();
+                r2 = Math.random();
+
+                Particle p = swarm.elementAt(i);
+
+                //Getting our pBest and gBest
+                pbestX = bestPositions.elementAt(i).getX();
+                pbestY = bestPositions.elementAt(i).getY();
+                gbestX = bestPositions.elementAt(globalBestIndex).getX();
+                gbestY = bestPositions.elementAt(globalBestIndex).getY();
+
+                //PSO Equations
+                newVelX = p.getV().getX() + r1*c1*(pbestX - p.getP().getX()) + r2*c2*(gbestX - p.getP().getX());
+                newVelY = p.getV().getY() + r1*c1*(pbestY - p.getP().getY()) + r2*c2*(gbestY - p.getP().getY());
+                newPosX = p.getP().getX() + p.getV().getX();
+                newPosY = p.getP().getY() + p.getV().getY();
+
+                //Setting new particle velocity and position
+                p.setP(newPosX,newPosY);
+                p.setV(newVelX, newVelY);
+
+                //updating pBest and gBest
+                //This should be evaluated in terms of fitness rather than position
+                if(newPosX > pbestX)
+                {
+                    bestPositions.insertElementAt(p.getP(), i);
+                    //Global best will only change when individual best changes too???
+                    findGBest();
+                }
+            }
+        }
+        for(int k=0; k<swarm.capacity(); k++) {
+            System.out.print(swarm.elementAt(k).getP().getX()+"["+k+"],\n");
         }
         System.out.println(globalBestIndex);
-
-
-        Particle p = new Particle();
-        Particle q = new Particle();
-        double newPosX, newPosY, newVelX, newVelY;
-        double r1,r2,c1,c2;
-        r1 = Math.random();
-        r2 = Math.random();
-        c1 = 1.05;
-        c2 = 1.05;
-
-        Position[] bestPositions = new Position[2];
-        bestPositions[0] = p.getP();
-        bestPositions[1] = q.getP();
-        double pbestX = bestPositions[0].getX();
-        double pbestY = bestPositions[1].getY();
-        double gbestX = 1;
-        double gbestY = 1;
-
-        newVelX = p.getV().getX() + r1*c1*(pbestX - p.getP().getX()) + r2*c2*(gbestX - p.getP().getX());
-        newVelY = p.getV().getY() + r1*c1*(pbestY - p.getP().getY()) + r2*c2*(gbestY - p.getP().getY());
-        newPosX = p.getP().getX() + p.getV().getX();
-        newPosY = p.getP().getY() + p.getV().getY();
-        p.setP(newPosX,newPosY);
-        p.setV(newVelX, newVelY);
     }
 }
