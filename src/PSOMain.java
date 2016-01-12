de2ae7db434524808c0b09fa6c5d490c7651e200
Niall Grogan <n.grogan2@nuigalway.ie>
2016-01-12 14:15:16 +0000

    Added method for exporting results for each topology to the same CSV file Note that lbest and von neumann do not appear to be working correctly

--- src/PSOMain.java
+++ src/PSOMain.java
@@ -2,21 +2,46 @@ import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 
-public class PSOMain {
+public class PSOMain implements Constants{
     public static void main(String[] Args)
     {
-        vonNeuPSO p = new vonNeuPSO();
-        p.initialise();
-        double[] results = p.execute();
+        double[][] results = new double[3][numIterations];
+        vonNeuPSO von = new vonNeuPSO();
+        von.initialise();
+        results[0] = von.execute();
+
+        lBestPSO l = new lBestPSO();
+        l.initialise();
+        results[1] = l.execute();
+
+        gBestPSO g = new gBestPSO();
+        g.initialise();
+        results[2] = g.execute();
+
         toCSVFile(results);
     }
 
-    private static void toCSVFile(double[] arr) {
+    private static void toCSVFile(double[][] arrDouble) {
         try {
-            BufferedWriter br = new BufferedWriter(new FileWriter("pso.csv"));
+            BufferedWriter br = new BufferedWriter(new FileWriter("PSO.csv"));
             StringBuilder sb = new StringBuilder();
-            for (double el : arr) {
-                sb.append(el);
+            sb.append("Von Neuman");
+            sb.append(", ");
+            sb.append("lBest");
+            sb.append(", ");
+            sb.append("gBest");
+            sb.append(",\n");
+
+            double[] arr0 = arrDouble[0];
+            double[] arr1 = arrDouble[1];
+            double[] arr2 = arrDouble[2];
+            for(int i=0; i<numIterations; i++)
+            {
+                sb.append(arr0[i]);
+                sb.append(", ");
+                sb.append(arr1[i]);
+                sb.append(", ");
+                sb.append(arr2[i]);
                 sb.append(",\n");
             }
             br.write(sb.toString());
