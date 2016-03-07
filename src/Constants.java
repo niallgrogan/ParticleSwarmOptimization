77f5efb68200534f30d506f82d2ef7a8b5ad9b77
Niall Grogan <n.grogan2@nuigalway.ie>
2016-03-07 20:51:45 +0000

    Some minor parameter changes

--- src/Constants.java
+++ src/Constants.java
@@ -1,12 +1,11 @@
-/**
- * Created by Niall on 18/11/2015.
- */
+
 public interface Constants {
     int numIterations = 2000;
-    int swarmSize = 50;
+    int swarmSize = 60;
     double c1 = 2.05;
     double c2 = 2.05;
     double constriction = 0.72984;
     double Vmax = 10000;
-    String[] functions = {"f4"};//{"Sphere", "Rosenbrock", "Ackley", "Griewank", "Rastrigin", "Schaffer(2D)", "Griewank(10D)"};
+    String[] functions = {"Sphere", "Rosenbrock", "Ackley", "Griewank", "Rastrigin", "Schaffer(2D)",
+            "f1","f2","f3","f4","f9", "Griewank(10D)"};
 }
