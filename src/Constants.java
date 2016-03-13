f5f954099b40db8f7ee0a3ec0f8aeb19762159ad
Niall Grogan <n.grogan2@nuigalway.ie>
2016-03-13 20:50:37 +0000

    Implementing method for running multiple topology tests

--- src/Constants.java
+++ src/Constants.java
@@ -1,11 +1,10 @@
 
 public interface Constants {
-    int numIterations = 2000;
+    int numIterations = 10000;
     int swarmSize = 60;
     double c1 = 2.05;
     double c2 = 2.05;
     double constriction = 0.72984;
     double Vmax = 10000;
-    String[] functions = {"Sphere", "Rosenbrock", "Ackley", "Griewank", "Rastrigin", "Schaffer(2D)",
-            "f1","f2","f3","f4","f9", "Griewank(10D)"};
+    String[] functions = {"Sphere", "Rosenbrock", "Ackley", "Griewank", "Rastrigin", "Schaffer(2D)","Griewank(10D)"};
 }
